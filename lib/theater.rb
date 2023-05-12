c9debcbbcfdb3b78bb44abfacc71dad903b7c936
unknown <2004230015ms@edu.oca.ac.jp>
2023-05-12 12:15:21 +0900

    追加料金を実装

--- lib/theater.rb
+++ lib/theater.rb
@@ -7,17 +7,25 @@ class Theater
         @child_price = 1000
     end
 
-    def price(name)
+    def price(name, premium=false, dolby=false)
+        additional_price = 0
+        if premium
+            additional_price += 1000
+        end
+        if dolby
+            additional_price += 200
+        end
+
         if name == "大学生"
-            return @college_price
+            return @college_price + additional_price
         elsif name == "高校生"
-            return @highschool_price
+            return @highschool_price + additional_price
         elsif name == "中/小学生"
-            return @jrhigh_elementary_price
+            return @jrhigh_elementary_price + additional_price
         elsif name == "幼児"
-            return @child_price
+            return @child_price + additional_price
         else
-            return @main_price
+            return @main_price + additional_price
         end
     end
 end
