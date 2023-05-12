4d510fc5f0ed3fe1ae0061c7f4888665cff439a6
unknown <2004230015ms@edu.oca.ac.jp>
2023-05-12 12:11:44 +0900

    大学生・高校生等の料金を実装

--- lib/theater.rb
+++ lib/theater.rb
@@ -1,9 +1,23 @@
 class Theater
     def initialize
         @main_price = 1900
+        @college_price = 1500
+        @highschool_price = 1000
+        @jrhigh_elementary_price = 1000
+        @child_price = 1000
     end
 
     def price(name)
-        @main_price
+        if name == "大学生"
+            return @college_price
+        elsif name == "高校生"
+            return @highschool_price
+        elsif name == "中/小学生"
+            return @jrhigh_elementary_price
+        elsif name == "幼児"
+            return @child_price
+        else
+            return @main_price
+        end
     end
 end
