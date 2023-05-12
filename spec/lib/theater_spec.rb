cc0a44c2ebf19da5b09310a7c97a1bb37e2e0335
unknown <2004230015ms@edu.oca.ac.jp>
2023-05-12 12:08:18 +0900

    大学生・高校生等の料金のテストを追加

--- spec/lib/theater_spec.rb
+++ spec/lib/theater_spec.rb
@@ -12,5 +12,28 @@ describe "料金・割引サービス表" do
             end
         end
     
+        context "大学生" do
+            it "1500円" do
+                expect(@theater.price("大学生")).to eq 1500
+            end
+        end
+    
+        context "高校生" do
+            it "1000円" do
+                expect(@theater.price("高校生")).to eq 1000
+            end
+        end
+    
+        context "中/小学生" do
+            it "1000円" do
+                expect(@theater.price("中/小学生")).to eq 1000
+            end
+        end
+    
+        context "幼児" do
+            it "1000円" do
+                expect(@theater.price("幼児")).to eq 1000
+            end
+        end
     end
 end
